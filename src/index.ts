278d39e8e1b6afb4a6ba39512aa90fe15308f053
PRAGA <dwwd>
2022-04-30 17:44:50 +0100

    👽️ First Commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { RabbitMqHealthIndicator } from "./rabbit-mq-health/indicator/rabbit-mq-health.heatlh";
+import { RabbitMqHealthModule } from "./rabbit-mq-health/rabbit-mq-health.module";
+
+export { RabbitMqHealthModule };
+export { RabbitMqHealthIndicator };
